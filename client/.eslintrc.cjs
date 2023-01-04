a598145f91e2ee99e5350478d20f9fe9ac3eecc7
HeRo3S <hungid6a1@gmail.com>
2023-01-05 06:45:35 +0700

    setup linters and code formatter

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
