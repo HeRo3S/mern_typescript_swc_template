c8d7bb74b024e26d3b0ca6084675e035f7f67460
HeRo3S <hungid6a1@gmail.com>
2023-01-05 09:53:17 +0700

    setup react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import routes from './utils/routes';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.component} />
+        ))}
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+export default WrappedApp;
---
+++ client/src/utils/routes.ts
@@ -0,0 +1,9 @@
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
+
+const routes = [
+  { path: '/', component: Home() },
+  { path: '*', component: NotFound() },
+];
+
+export default routes;
